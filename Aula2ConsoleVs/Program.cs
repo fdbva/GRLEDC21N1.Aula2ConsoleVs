a528b5d3905fd71fa104713f34b70c29be266e0b
fdbva <villela.felipe@gmail.com>
2021-04-23 21:53:58 -0300

    Add project files.

---
+++ Aula2ConsoleVs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula2ConsoleVs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started!");
+
+            Console.Write("Enter a number: ");
+
+            var numberText = Console.ReadLine();
+
+            var parseDidntWork = !Int32.TryParse(numberText, out var number);
+
+            if (parseDidntWork)
+            {
+                Console.WriteLine("NaN!");
+                Console.ReadKey();
+                return;
+            }
+
+            if (number == 1)
+            {
+                Console.WriteLine("Number is one!");
+            }
+            else
+            {
+                Console.WriteLine("Not one!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
